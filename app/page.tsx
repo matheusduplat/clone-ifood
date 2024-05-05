e539fb3da901c89d04c57a5877a1048f5345f46c
matheusduplat <matheusduplat@hotmail.com>
2024-05-05 12:22:26 -0300

    feat: promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { CategoryList } from "./@core/components/cateroryList";
 import { Header } from "./@core/components/header";
 import { Search } from "./@core/components/search";
@@ -11,6 +12,17 @@ export default function Home() {
       </div>
       <div className="px-5 pt-3">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/banner-promo-0.png"
+          alt="Até 30% de desconto"
+          className="h-auto w-full object-contain"
+          width={0}
+          height={0}
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
