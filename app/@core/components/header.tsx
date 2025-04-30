727b367e9d21a8ca0f7d226244c108d888d43cff
Matheus Duplat <matheus.duplat@soterotech.com.br>
2025-04-30 17:15:52 -0300

    aula 4 36:06

--- app/@core/components/header.tsx
+++ app/@core/components/header.tsx
@@ -1,9 +1,11 @@
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 export function Header() {
+  const { data } = useSession();
   return (
     <div className="flex justify-between px-5 pt-6">
       <div className="relative h-[30px] w-[100px]">
@@ -16,6 +18,15 @@ export function Header() {
           />
         </Link>
       </div>
+      {data?.user?.name ? (
+        <div className="flex">
+          {data.user.name}
+          <Button onClick={() => signOut()}>Sair</Button>
+        </div>
+      ) : (
+        <Button onClick={() => signIn()}>Login</Button>
+      )}
+
       <Button
         size="icon"
         variant="outline"
